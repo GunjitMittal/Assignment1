3ce2482d9cf328f758535aa123ffdcc2adc174a7
GunjitMittal <101900401+GunjitMittal@users.noreply.github.com>
2022-04-02 13:24:34 +0530

    Add files via upload

--- Assignment1/codes/main.py
+++ Assignment1/codes/main.py
@@ -4,23 +4,25 @@ import numpy as np
 import math
 plt.grid()
 pi=math.pi
-a=math.tan(pi/3)
-b=math.tan(pi/4)
-c=(a+b)*60
+h=60
+theta1=pi/3
+theta2=pi/4
+a=math.tan(theta1)
+b=math.tan(theta2)
+c=(a+b)*h
 plt.plot(a,b)
-plt.annotate("Light house",[3,59])
-plt.annotate("A",[60*a+1,1])
-plt.annotate("B",[-60*b+3,1])
-plt.annotate("60\N{DEGREE SIGN}",[2,53])
-plt.annotate("45\N{DEGREE SIGN}",[-9,53])
-plt.annotate("60m",[1,30])
-plt.annotate("60"+b'\xfb'.decode('cp437')+"3m",[50,1])
-plt.annotate("60m",[-50,1])
-plt.plot([-60*b,60*a],[0,0],marker='o')
-plt.plot([-60*b,0],[0,60],marker='o')
-plt.plot([0,60*a],[60,0],marker='o')
-plt.plot([0,0],[60,0],marker='o')
-mp.Arc([0,0],1,1,angle=60,theta1=270)
+plt.annotate("Light house",[3,h-1])
+plt.annotate("A",[h*a+1,1])
+plt.annotate("B",[-h*b+3,1])
+plt.annotate("60\N{DEGREE SIGN}",[2,h-7])
+plt.annotate("45\N{DEGREE SIGN}",[-9,h-7])
+plt.annotate("60m",[1,h/2])
+plt.annotate("60"+b'\xfb'.decode('cp437')+"3m",[h*a/2,1])
+plt.annotate("60m",[-h*b/2,1])
+plt.plot([-h*b,h*a],[0,0],marker='o')
+plt.plot([-h*b,0],[0,h],marker='o')
+plt.plot([0,h*a],[h,0],marker='o')
+plt.plot([0,0],[h,0],marker='o')
 plt.show()
 if(164==round(c)):
     print("proved")
