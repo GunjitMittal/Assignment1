ee89361720b2d76fad52d7e2f64fa76af1a227dc
GunjitMittal <101900401+GunjitMittal@users.noreply.github.com>
2022-04-03 14:19:11 +0530

    Add files via upload

--- Assignment1/codes/main.py
+++ Assignment1/codes/main.py
@@ -9,19 +9,20 @@ theta1=pi/3
 theta2=pi/4
 a=math.tan(theta1)
 b=math.tan(theta2)
-c=(a+b)*h
-plt.plot(a,b)
+x=a*h
+y=b*h
+c=x+y
 plt.annotate("Light house",[3,h-1])
-plt.annotate("A",[h*a+1,1])
-plt.annotate("B",[-h*b+3,1])
+plt.annotate("A",[x+1,1])
+plt.annotate("B",[-y+3,1])
 plt.annotate("60\N{DEGREE SIGN}",[2,h-7])
 plt.annotate("45\N{DEGREE SIGN}",[-9,h-7])
 plt.annotate("60m",[1,h/2])
-plt.annotate("60"+b'\xfb'.decode('cp437')+"3m",[h*a/2,1])
-plt.annotate("60m",[-h*b/2,1])
-plt.plot([-h*b,h*a],[0,0],marker='o')
-plt.plot([-h*b,0],[0,h],marker='o')
-plt.plot([0,h*a],[h,0],marker='o')
+plt.annotate("60"+b'\xfb'.decode('cp437')+"3m",[x/2,1])
+plt.annotate("60m",[-y/2,1])
+plt.plot([-y,x],[0,0],marker='o')
+plt.plot([-y,0],[0,h],marker='o')
+plt.plot([0,x],[h,0],marker='o')
 plt.plot([0,0],[h,0],marker='o')
 plt.show()
 if(164==round(c)):
