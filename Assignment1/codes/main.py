00d295352a513923b5d65afcf028b4ed3c5510be
GunjitMittal <101900401+GunjitMittal@users.noreply.github.com>
2022-04-08 21:34:31 +0530

    Add files via upload

--- Assignment1/codes/main.py
+++ Assignment1/codes/main.py
@@ -1,15 +1,19 @@
 import matplotlib.pyplot as plt
 import matplotlib.patches as mp
-import numpy as np
 import math
+import sympy as smp
 plt.grid()
 pi=math.pi
-h=60
-theta1=pi/3
-theta2=pi/4
-x=math.tan(theta1)*h
-y=math.tan(theta2)*h
+h=smp.symbols('h')
+theta1=smp.symbols('theta1')
+theta2=smp.symbols('theta2')
+x=smp.tan(theta1)*h
+y=smp.tan(theta2)*h
 answer=x+y
+answer = answer.subs({h:60, theta1: pi/3, theta2 : pi/4})
+x = x.subs({h:60, theta1: pi/3})
+y = y.subs({h:60, theta2: pi/4})
+h = h.subs({h:60})
 plt.annotate("Light house",[3,h-1])
 plt.annotate("A",[x+1,1])
 plt.annotate("B",[-y+3,1])
